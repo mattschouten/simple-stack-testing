0f4cc9f2148539a00cb977e5225273183a29d159
Matt Schouten <matt.schouten@ibm.com>
2026-04-13 12:24:15 -0500

    Remove local resources

--- modules/manifest-module/output.tf
+++ modules/manifest-module/output.tf
@@ -2,3 +2,13 @@ output "static_timestamp" {
   description = "Manifest timestamp"
   value       = time_static.static_timestamp.rfc3339
 }
+
+output "manifest_filename" {
+  description = "Filename that would be assigned to manifest file"
+  value       = time_static.static_timestamp.rfc3339
+}
+
+output "manifest_timestamp" {
+  description = "Contents that would be put in manifest file"
+  value       = "Manifest file created by Terraform Stacks Tutorial at ${time_static.static_timestamp.rfc3339}"
+}
--- modules/pet-module/output.tf
+++ modules/pet-module/output.tf
@@ -1,4 +1,9 @@
 output "pet_names" {
-  description = "Names of pets / files created"
+  description = "Names of pets created"
   value       = random_pet.pet[*].id
+}
+
+output "pet_filenames" {
+  description = "Names of files that would be created in the environment / pets directory"
+  value       = [for p in random_pet.pet : "${var.environment_parent_directory}/${var.target_environment}/pets/${p.id}"]
 }
