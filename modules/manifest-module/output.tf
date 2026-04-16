91cf400bf1e1a72cdce40746824f54a169f4483c
Matt Schouten <matt.schouten@ibm.com>
2026-04-16 10:44:11 -0500

    Further output tidying

--- modules/manifest-module/output.tf
+++ modules/manifest-module/output.tf
@@ -10,5 +10,5 @@ output "manifest_filename" {
 
 output "manifest_contents" {
   description = "Contents of manifest file"
-  value = "Manifest file created by Terraform Stacks Tutorial at ${time_static.static_timestamp.rfc3339}:  manifest.txt plus ${length(var.additional_files)} additional files"
+  value = "Manifest file originally created by Terraform Stacks Tutorial at ${time_static.static_timestamp.rfc3339}:  manifest.txt plus ${length(var.additional_files)} additional files"
 }
