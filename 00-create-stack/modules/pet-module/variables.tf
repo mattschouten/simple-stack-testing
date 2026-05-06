af3d9012e5d8e3dbae195666b409232810c7b5d1
Matt Schouten <matt.schouten@ibm.com>
2026-05-06 15:23:02 -0500

    Add new step 00 to create the configuration

--- 00-create-stack/modules/pet-module/variables.tf
+++ 00-create-stack/modules/pet-module/variables.tf
@@ -1,17 +1,17 @@
 variable "parent_directory" {
   description = "Parent directory environment directories are placed within"
   type        = string
-  default     = "stacks_tutorial"
+  default     = "ROOT"
 }
 
 variable "environment_name" {
-  description = "Environment to deploy into"
+  description = "Environment to deploy"
   type        = string
-  default     = "DEFAULT_TARGET"
+  default     = "unknown_env"
 }
 
 variable "pet_count" {
   description = "Number of pets to create"
   type        = number
-  default     = 1
+  default     = 3
 }
